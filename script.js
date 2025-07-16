b3f7873d89b229c4f631c652117bf3df88c51c9f
Bruna D' Oliveira <brunadoliveira.arq@gmail.com>
2025-07-16 12:03:53 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,5 @@
+function enviarFormulario(event) {
+  event.preventDefault();
+  alert("Obrigado! Seu eBook será enviado em breve para seu e-mail.");
+  // Aqui você pode enviar os dados para um servidor via fetch/AJAX se quiser
+}
